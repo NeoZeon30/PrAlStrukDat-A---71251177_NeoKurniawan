5d97cb45bcfdda1d17697e2fb2e4a8f2d82f9ca6
NeoZeon30 <neokrnwn30@gmail.com>
2026-09-03 15:43:26 +0700

    Pola Sakit Kepala

---
+++ Pola_sakit.py
@@ -0,0 +1,25 @@
+def pola_sakit_kepala(panjang, lebar):
+    panjang = abs(panjang)
+    lebar = abs(lebar)
+
+    if panjang != lebar:
+        print("Panjang dan lebar harus sama!!")
+    
+    elif panjang % 2 == 0 or lebar % 2 == 0:
+        print("Panjang dan lebar harus bilangan ganjil!!")
+
+    else:
+        tengah = panjang // 2
+
+        for i in range(panjang):
+            for j in range(lebar):
+                nilai = 1 + (i - tengah) + (j - tengah)
+                                
+                digit = nilai % 10
+                
+                if j == lebar - 1:
+                    print(digit)
+                else:
+                    print(digit, end=" ")
+                
+pola_sakit_kepala(7, 7)
